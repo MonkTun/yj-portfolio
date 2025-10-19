00d500cac872cae58d5dba123f2c4c30378a382a
Youngje Park <peroni014@gmail.com>
2025-10-19 13:59:17 -0700

    changed json

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "./", ///yj-portfolio
+  base: "/yj-portfolio", ///yj-portfolio
 });
